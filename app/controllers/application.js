69c97c159a362778172b45a9bbcb8f14cb7cd80f
rain0822 <(none)>
2017-12-11 19:05:48 +0800

    version 2

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,24 +6,43 @@ export default Controller.extend({
     seconds: '32',
     ytVideos: [{
         id: 'Jv3zvWZlXkk',
-        title: '終於結束的起點',
-        emberYoutube: 'emberYoutube0'
+        title: '終於結束的起點'
     },{
         id: 'pd3eV-SG23E',
-        title: '後來的我們',
-        emberYoutube: 'emberYoutube1'
+        title: '後來的我們'
     },{
         id: '38lcQsEMGrk',
-        title: '我不願讓你一個人',
-        emberYoutube: 'emberYoutube2'
+        title: '我不願讓你一個人'
     },{
         id: '_o0oeyCtoFA',
-        title: '知足',
-        emberYoutube: 'emberYoutube3'
+        title: '知足'
+    },{
+        id: 'Y-0Ra3PvTWg',
+        title: '乾杯'
     },{
         id: 'zBJjFEE0rUc',
-        title: '突然好想你',
-        emberYoutube: 'emberYoutube4'
+        title: '突然好想你'
+    },{
+        id: 'znhb-_KgLoM',
+        title: '傷心的人別聽慢歌'
+    },{
+        id: 'zdF9gOpFoIQ',
+        title: '派對動物'
+    },{
+        id: 'jQE6biHTDow',
+        title: '離開地球表面'
+    },{
+        id: 'hr8jWDyb1jg',
+        title: '你不是真正的快樂'
+    },{
+        id: 'R2s-H_crYkc',
+        title: '倔強'
+    },{
+        id: 'iSkRGgYSQfY',
+        title: '戀愛ing'
+    },{
+        id: '5VUUGZ1-nlY',
+        title: '志明與春嬌'
     }],
     playerVars: {
         autoplay: 1,
